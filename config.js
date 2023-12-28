008877bc27266c72dbc768844fc5561c44ad1cf3
Cuong <cuongchamchi1904@gmail.com>
2023-12-28 11:50:00 +0700

    hpbd

---
+++ config.js
@@ -0,0 +1,28 @@
+var config = {
+    texts: [
+        "Chúc mừng sinh nhật",      
+        "Nguyễn Thị Loan",  
+        "Chúc chị luôn luôn",
+        "Vui vẻ",
+        "Xinh đẹp",
+        "Hạnh phúc",
+        "Sớm tìm ra con đường mìn mình thích",
+        "Hẹn chị hôm nay",
+        "Gặp nhau ở",
+        "Bình nguyên vô vọng",
+        "==))",
+    ],
+    imgs: {
+        "Nguyễn Thị Loan": "./imgs/loan.png",
+    },
+    desc: {
+        turn_on: "Bắt đầu",
+        play: "Nhạc nè",
+        bannar_coming: "Bấm tiếp nè",
+        balloons_flying: "Thiếu cái gì đó",
+        cake_fadein: "Bánh ư？",
+        light_candle: "Nến ư？",
+        wish_message: "Chúc mừng sinh nhật",
+        story: "Tin nhắn dành cho bạn",
+    }
+};
